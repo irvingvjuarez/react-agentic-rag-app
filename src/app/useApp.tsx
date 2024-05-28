2cdc16cec0aa9b9c7b8f3823c448e61434ebb98b
Irving Juárez <irving.juarez@thomsonreuters.com>
2024-05-28 17:48:51 -0600

    Hello world endpoint created and consumed from the frontend

--- src/app/useApp.tsx
+++ src/app/useApp.tsx
@@ -1,8 +1,19 @@
 import { FormEvent } from "react";
 
+async function fetchData() {
+    const response = await fetch('/api/hello');
+    const data = await response.json();
+    return data;
+}
+
 export default function useApp() {
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        try {
+            fetchData().then(data => console.log(data));
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return {
