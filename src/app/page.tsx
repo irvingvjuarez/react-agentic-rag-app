416dbae237be3b1dba0dd0e36da5f97f2ac0dc40
Irving Juárez <irving.juarez@thomsonreuters.com>
2024-05-28 16:23:06 -0600

    Design created with tailwind for chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex m-4 justify-center items-center">
-      <section className="flex-row flex-center space-y-2 w-full max-w-screen-sm text-sm">
-        <article className="w-full h-[82vh] bg-slate-100 rounded-md md:h-[60vh]">
+    <main className="flex m-4 justify-center items-center h-screen w-screen">
+      <section className="p-2 grid grid-rows-[92%,8%] flex-center space-y-2 text-sm h-full w-full max-w-[700px]">
+        <article className="w-full bg-slate-800 rounded-md">
 
         </article>
 
-        <form className="flex p-2 bg-slate-100 rounded-md space-x-2">
+        <form className="p-2 bg-slate-800 rounded-md space-x-2">
           <input type="text" placeholder="Type a message" className="rounded-md shadow-slate-700 shadow-inner p-1" />
           <button className="button-gradient text-white rounded-md leading-normal p-1">Send message</button>
         </form>
