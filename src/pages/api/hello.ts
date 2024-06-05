f49d35cf9581bf24b9dc90fa7d87896ebebdcae4
Irving Juárez <irvingjuarez274@gmail.com>
2024-06-04 18:40:31 -0600

    New data store added along with the router for query engines

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,48 +1,30 @@
-import { OpenAI, OpenAIEmbedding, ResponseSynthesizer, RetrieverQueryEngine, serviceContextFromDefaults, SimpleDirectoryReader, SimpleResponseBuilder, VectorIndexRetriever, VectorStoreIndex } from "llamaindex";
+import { RouterQueryEngine, SimpleDirectoryReader, VectorStoreIndex } from "llamaindex";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        // Detailed process about how to create a RAG app
-        const documents = await new SimpleDirectoryReader().loadData({directoryPath: './data'})
-        const index = await VectorStoreIndex.fromDocuments(documents);
+        const beginningSpaceXData = await new SimpleDirectoryReader().loadData({directoryPath: './data'});
+        const beginningSpaceXIndex = await VectorStoreIndex.fromDocuments(beginningSpaceXData);
+        const beginningSpaceXQueryEngine = beginningSpaceXIndex.asQueryEngine();
 
-        const customLLM = new OpenAI();
-        const customEmbedding = new OpenAIEmbedding();
-        const customServiceContext = serviceContextFromDefaults({
-            llm: customLLM,
-            embedModel: customEmbedding
+        const futureSpaceXData = await new SimpleDirectoryReader().loadData({directoryPath: './data-source2'});
+        const futureSpaceXIndex = await VectorStoreIndex.fromDocuments(futureSpaceXData);
+        const futureSpaceXQueryEngine = futureSpaceXIndex.asQueryEngine();
+
+        const queryEngine = RouterQueryEngine.fromDefaults({
+            queryEngineTools: [
+                {
+                    queryEngine: beginningSpaceXQueryEngine,
+                    description: 'Beginnings of space exploration'
+                },
+                {
+                    queryEngine: futureSpaceXQueryEngine,
+                    description: 'Future of space exploration'
+                }
+            ]
         })
-
-        const assistantInitialPropmt = ({context = '', query = ''}) => {
-            return `
-                Context information is below wrapped in triple quotes:
-                """${context}"""
-
-                Given the context information, answer the query
-                At the end of the response, don't forget to add a farewell message from Jarvis, the AI assistant.
-                Query: ${query}
-                Answer: 
-            `;
-        }
-
-        const responseBuilder = new SimpleResponseBuilder(customServiceContext, assistantInitialPropmt);
-        const customSynthesizer = new ResponseSynthesizer({
-            responseBuilder,
-            serviceContext: customServiceContext
-        });
-        const customRetriever = new VectorIndexRetriever({
-            index
-        })
-
-        const customQueryEngine = new RetrieverQueryEngine(
-            customRetriever,
-            customSynthesizer
-        )
-
-        const request = await customQueryEngine.query(req.body);
+        const request = await queryEngine.query(req.body)
         const response = request.toString();
-        
         res.status(200).json({response});
     } else {
         res.status(405).json({ error: 'Method Not Allowed' })
